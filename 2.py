d41f09fe637064b19769a4148968b750f85fe2e5
AmelinaDiana <91514746+AmelinaDiana@users.noreply.github.com>
2022-09-13 20:09:08 +0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,9 @@
+import pyautogui
+pyautogui.moveTo(25, 1055)
+pyautogui.click()
+pyautogui.moveTo(100, 520)
+import time
+time.sleep(1)
+pyautogui.click()
+time.sleep(1)
+pyautogui.write('hfpevty kb z?') 
