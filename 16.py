f639a79e65ee0f07a8e45e8f3c5a0de0934e7484
AmelinaDiana <91514746+AmelinaDiana@users.noreply.github.com>
2022-10-18 20:27:00 +0700

    Add files via upload

---
+++ 16.py
@@ -0,0 +1,28 @@
+import pyautogui
+import time
+num = input('Введите число от 1 до 8: ')
+pyautogui.hotkey('alt', 'tab')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 'n')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 't')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 't')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 't')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 't')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 't')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 't')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 't')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 't')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', 't')
+time.sleep(0.5)
+pyautogui.hotkey('ctrl', str(num))
+
+
