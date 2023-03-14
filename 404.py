ef2f7a51e6e2fdff900bb8564cb5d6ae87844d36
AmelinaDiana <91514746+AmelinaDiana@users.noreply.github.com>
2023-03-14 20:16:23 +0700

    Add files via upload

---
+++ 404.py
@@ -0,0 +1,14 @@
+comand = input("Введите команду").lower()
+
+while comand != "выход":
+    if comand == "стишок":
+        print("глаз твоих бездонных")
+        print("манит глубина")
+        print("ты сходи проверь-ка")
+        print("глубь глазного дна")
+    elif comand == "краборифма":
+        slovo = input("Введите любое слово")
+        print("крабо"+slovo)
+    
+    command = input("Введите команду").lower()
+print("Чат-бот закончил")
